ec9069a7b8a2d67ddefd56e492332e5aa44c883b
Ben Rogers <rogers@farmerrogers.com>
2013-04-16 09:56:23 -0700

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinalShouter::Application.config.secret_token = '62d6f75584d004c5254bea19f8e35e90377c654b345d595c739539d9e4504ef8044c52ca38927df3f16d16b601860bbbbc69d8bea2c1a71072024366f4b0ffbf'
